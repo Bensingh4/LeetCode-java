5b45321f1ddf65823316333a9866103668c247ee
Bensingh jayananth <147735591+Bensingh4@users.noreply.github.com>
2026-09-09 11:41:08 +0530

    Time: 1 ms (77.62%), Space: 42.3 MB (66.35%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    public boolean isHappy(int n) {
+
+        int slow = square(n);
+        int fast = square(square(n));
+
+        while (slow != fast) {
+
+            slow = square(slow);
+            fast = square(square(fast));
+        }
+
+        return slow == 1;
+    }
+
+    public int square(int n) {
+
+        int sum = 0;
+
+        while (n > 0) {
+
+            int digit = n % 10;
+
+            sum = sum + digit * digit;
+
+            n = n / 10;
+        }
+
+        return sum;
+    }
+}
